da00631724507c63a8d0c93d8f860d9470364542
akshay2408 <akshaybabar2408@gmail.com>
2022-12-08 17:25:20 +0530

    Refactor payment page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@
 
 # ApplicationHelper
 module ApplicationHelper
+  def project_applicants(projects, project_title)
+    project = projects.find_by(title: project_title)
+    applicants = project.applicants.where(status: 'approved').pluck(:name).join(', ')
+  end
 end
