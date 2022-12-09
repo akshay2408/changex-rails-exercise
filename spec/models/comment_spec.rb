551ea22f010bd810041a7240cc4e72e3b070fc11
akshay2408 <akshaybabar2408@gmail.com>
2022-12-09 16:02:11 +0530

    Added Rspec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Comment do
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to validate_presence_of(:current_status) }
+    it { is_expected.to validate_presence_of(:changed_status) }
+  end
+
+  describe 'Associations' do
+    it { should belong_to(:applicant) }
+  end
 end
